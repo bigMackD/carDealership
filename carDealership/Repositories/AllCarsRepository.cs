4e6bc4addf2e6460b1516214b318d29fe969cf5b
Maciek <maciek.d@me.com>
2018-08-02 21:20:03 +0200

    Added repository class

---
+++ carDealership/Repositories/AllCarsRepository.cs
@@ -0,0 +1,24 @@
+﻿using carDealership.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carDealership.Repositories
+{
+    class AllCarsRepository
+    {
+        private static List<Car> _carRepository = new List<Car>();
+
+        public void AddCar(Car car)
+        {
+            _carRepository.Add(car);
+        }
+
+        public IEnumerable<Car> GetAllCars()
+        {
+            return _carRepository;
+        }
+    }
+}
---
+++ carDealership/Repositories/AllTransactionsRepository.cs
@@ -0,0 +1,24 @@
+﻿using carDealership.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carDealership.Repositories
+{
+    public class AllTransactionsRepository
+    {
+        private static List<Transaction> _allTransactions = new List<Transaction>();
+
+        public void AddTransaction(Transaction transaction)
+        {
+            _allTransactions.Add(transaction);
+        }
+        public IEnumerable<Transaction> GetAllTransactions()
+        {
+            return _allTransactions;
+        }
+
+    }
+}
