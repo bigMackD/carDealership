bd559308782d0c49180d71033ec6b377fca8676e
Maciek <maciek.d@me.com>
2018-08-02 21:19:12 +0200

    Added class factories

---
+++ carDealership/Factories/CarFactory.cs
@@ -0,0 +1,60 @@
+﻿using carDealership.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carDealership.Factories
+{
+    class CarFactory
+    {
+        public Car CreateCar()
+        {
+            Car createdCar = null;
+            Console.WriteLine("Choose car type: ");
+            Console.WriteLine("1.Sport car");
+            Console.WriteLine("Truck");
+            var chosenCar = Console.ReadKey(true);
+            switch (chosenCar.Key)
+            {
+                case ConsoleKey.D1:
+                    createdCar = CreateSportCar();
+                    break;
+                case ConsoleKey.D2:
+                    createdCar = CreateTruck();
+                    break;
+            }
+            return createdCar;
+        }
+
+        private Car CreateSportCar()
+        {
+            var sportCar = new SportCar();
+            Console.WriteLine("Horsepower: ");
+            sportCar.HorsePower = int.Parse(Console.ReadLine());
+            return sportCar;
+
+        }
+
+        private Car CreateTruck()
+        {
+            var truck = new Truck();
+            Console.WriteLine("Number of wheels: ");
+            truck.NumberOfWheels = int.Parse(Console.ReadLine());
+            return truck;
+        }
+
+        private void FillCarFields(Car car)
+        {
+
+            Console.WriteLine("Model: ");
+            car.Model = Console.ReadLine();
+            Console.WriteLine("Type: ");
+            car.Type = Console.ReadLine();
+            Console.WriteLine("Number of doors: ");
+            car.NumberOfDoors = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
---
+++ carDealership/Factories/TransactionFactory.cs
@@ -0,0 +1,69 @@
+﻿using carDealership.Models;
+using carDealership.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carDealership.Factories
+{
+    class TransactionFactory
+    {
+        public Transaction CreateTransaction(Car car, bool isIncoming)
+        {
+            Transaction createdTransaction = null;
+            Console.WriteLine("Bill or invoice?: ");
+            Console.WriteLine("1.Bill");
+            Console.WriteLine("2.Invoice");
+            var chosenTransaction = Console.ReadKey(true);
+            switch (chosenTransaction.Key)
+            {
+                case ConsoleKey.D1:
+                    createdTransaction = CreateBill(car,isIncoming);
+                    break;
+                case ConsoleKey.D2:
+                    createdTransaction = CreateFvat(car,isIncoming);
+                    break;
+            }
+            return createdTransaction;
+        }
+
+        private Transaction CreateBill(Car car,bool isIncoming)
+        {
+            var bill = new Bill();
+            FillTransactionFields(bill, car, isIncoming);
+            Console.WriteLine("Enter nip: ");
+            bill.Nip = int.Parse(Console.ReadLine());
+            return bill;
+        }
+
+        private Transaction CreateFvat(Car car,bool isIncoming)
+        {
+            var fvat = new Fvat();
+            FillTransactionFields(fvat,car,isIncoming);
+            Console.WriteLine("Enter precentage: ");
+            fvat.Percentage = int.Parse(Console.ReadLine());
+            fvat.CompanyName = Console.ReadLine();
+            return fvat;
+        }
+
+        public virtual void FillTransactionFields(Transaction transaction, Car car, bool isIncoming)
+        {
+            Console.WriteLine($"Date: ");
+            // parse to datetime
+            Console.WriteLine($"Model of car: ");
+            transaction.Car = car;
+            if(isIncoming == true)
+            {
+                transaction.IsIncoming = true;
+            }
+            else
+            {
+                transaction.IsIncoming = false;
+            }
+            Console.WriteLine($"Value of transaction: ");
+            transaction.Price = int.Parse(Console.ReadLine());
+        }
+    }
+}
