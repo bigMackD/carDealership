06ef79615e547b26406ccb1ae2388f34b42989cf
Maciek <maciek.d@me.com>
2018-08-03 18:21:00 +0200

    Added class, that protects from invalid input

---
+++ carDealership/Helpers/InputHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carDealership.Helpers
+{
+    public class InputHelper
+    {
+        
+        public int getIntFromConsole(string message)
+        {
+            Console.WriteLine(message);
+            var input = Console.ReadLine();
+            if(int.TryParse(input, out int result))
+            {
+                return result;
+            }
+            else
+            {
+                Console.WriteLine("Wrong input format. Try again!");
+                return getIntFromConsole(message);
+            }
+        }
+    }
+}
